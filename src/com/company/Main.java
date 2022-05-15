7f584e70cabc39f104502a26db0559cdc629ab7e
aselisak <kiyatova1984@gmail.com>
2022-05-15 12:05:16 -0400

    the red car is already with some basic feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        BasicFeatures car = new BasicFeatures();
-        
+        BasicFeatures audi = new BasicFeatures();
+       audi.color="red";
+       audi.doors="4";
+       audi.engineCapac="V8";
+       audi.headLights="blue";
+       audi.regNo="123654";
+
     }
 }
